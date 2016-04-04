fdfb0005a0034d8439e81cbffb3696c9f0d097ee
premprakashsingh <premsingh.singh386@gmail.com>
2016-04-04 20:20:19 +0000

    completed project added timestamp's route

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
@@ -26,8 +26,50 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 routes(app, passport);
-
+*/
+var express=require('express');
+var path=require("path");
+var app=express();
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
+});
+app.get('/',function(req,res){
+	//res.send("hello world");
+	//res.render('index');
+	res.sendFile(path.join(__dirname+'/public/default.html'));
+});
+app.get("/:indate([0-9]+)",function(req,res){
+	var para=req.params.indate;
+	var out={};
+	var d=new Date(Number(para*1000));
+	if(d){
+		var natdate=d.toDateString().split(" ");
+		natdate=natdate.slice(1);
+		natdate=natdate.join(" ");
+		
+		out["natural"]=natdate;
+		out["unix"]=para;
+	res.send(JSON.stringify(out));
+	}
+	else{
+		out["natural"]=null;
+		out["unix"]=null;
+	res.send(JSON.stringify(out))
+	}
+});
+app.all("/*",function(req,res){
+	var para=req.params[0];
+	var out={};
+	var d=new Date(para);
+	if(d){
+		out["natural"]=para;
+		out["unix"]=d.getTime()/1000;
+	res.send(JSON.stringify(out));
+	}
+	else{
+		out["natural"]=null;
+		out["unix"]=null;
+	res.send(JSON.stringify(out))
+	}
 });
